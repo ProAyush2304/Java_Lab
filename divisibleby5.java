16acd49ceafc6f9f891e3d2c3a1ae86449528d7f
ProAyush2304 <academicalspace23@gmail.com>
2026-05-01 18:12:32 +0530

    Add Java program to check divisibility by 5

---
+++ divisibleby5.java
@@ -0,0 +1,9 @@
+class Divisible{
+    public static void main(String[] args) {
+        int n=40;
+        if(n%5==0)
+            System.out.println("Divisible by 5!");
+        else
+            System.out.println("Not divisible by 5!");
+    }
+}
